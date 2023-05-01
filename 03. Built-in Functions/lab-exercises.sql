254be0033094ea9547eb2207f18c727b528ba0c7
AleksKostadinov <kostadinov12@gmail.com>
2023-05-01 21:15:34 +0300

    Exercises

---
+++ 03. Built-in Functions/lab-exercises.sql
@@ -0,0 +1,16 @@
+SELECT `title` FROM `books`
+WHERE SUBSTRING(`title`, 1, 3) = "The";
+
+SELECT `title` FROM `books`
+WHERE LOCATE("The", `title`) = 1;
+
+SELECT REPLACE(`title`, "The", "***") AS `title` FROM `books`
+WHERE SUBSTRING(`title`, 1, 3) = "The";
+
+SELECT ROUND(SUM(`cost`), 2) FROM `books`;
+
+SELECT CONCAT_WS(" ", `first_name`, `last_name`) AS "Full Name",
+TIMESTAMPDIFF(DAY, `born`, `died`) AS  "Days Lived" FROM `authors`;
+
+SELECT `title` FROM `books` WHERE `title` LIKE "Harry Potter%";
+
