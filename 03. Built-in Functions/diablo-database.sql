592f49348514e3547ff5b1c3bd4556e3e87d91b9
AleksKostadinov <kostadinov12@gmail.com>
2023-05-02 00:06:51 +0300

    Exercises

---
+++ 03. Built-in Functions/diablo-database.sql
@@ -0,0 +1,36 @@
+# Games from 2011 and 2012 Year
+SELECT `name`, date_format(`start`, "%Y-%m-%d") AS `start` FROM `games`
+WHERE YEAR(`start`) IN (2011, 2012)
+ORDER BY `start`, `name`
+LIMIT 50;
+
+
+# User Email Providers
+SELECT `user_name`, 
+SUBSTRING(`email`, LOCATE("@", `email`) + 1) AS `email provider` 
+FROM `users`
+ORDER BY `email provider`, `user_name`;
+
+
+# Get Users with IP Address Like Pattern
+SELECT `user_name`, `ip_address` FROM `users`
+WHERE `ip_address` LIKE "___.1%.%.___"
+ORDER BY `user_name`;
+
+
+# Show All Games with Duration and Part of the Day
+SELECT `name` AS `game`, 
+CASE
+WHEN HOUR(`start`) >= 0 AND HOUR(`start`) < 12 THEN "Morning"
+WHEN HOUR(`start`) >= 12 AND HOUR(`start`) < 18 THEN "Afternoon"
+ELSE "Evening"
+END
+AS `Part of the Day`, 
+CASE 
+WHEN `duration` <= 3 THEN "Extra Short"
+WHEN `duration` <= 6 THEN "Short"
+WHEN `duration` <= 10 THEN "Long"
+ELSE "Extra Long"
+END
+AS `Duration`
+FROM `games`;
