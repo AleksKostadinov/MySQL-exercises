a267ad324d25ceb08123d520e8474db8721942b0
AleksKostadinov <kostadinov12@gmail.com>
2023-05-02 22:56:56 +0300

    Exercises

--- 05. Table Relations/lab-exercises.sql
+++ 05. Table Relations/lab-exercises.sql
@@ -47,3 +47,35 @@ CREATE TABLE `peaks` (
         REFERENCES `mountains` (`id`)
         ON DELETE CASCADE
 );
+
+# Project Management DB
+CREATE TABLE `clients` (
+    `id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+    `client_name` VARCHAR(100)
+);
+
+CREATE TABLE `projects` (
+    `id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+    `client_id` INT(11),
+    `project_leader_id` INT(11)
+);
+
+CREATE TABLE `employees` (
+    `id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+    `first_name` VARCHAR(30),
+    `last_name` VARCHAR(30),
+    `project_id` INT(11)
+);
+
+ALTER TABLE `projects`
+ADD CONSTRAINT `fk_projects_clients`
+FOREIGN KEY (`client_id`)
+REFERENCES `clients`(`id`),
+ADD CONSTRAINT `fk_projects_employees`
+FOREIGN KEY (`project_leader_id`)
+REFERENCES `employees`(`id`);
+
+ALTER TABLE `employees`
+ADD CONSTRAINT `fk_employees_projects`
+FOREIGN KEY (`project_id`)
+REFERENCES `projects`(`id`);
